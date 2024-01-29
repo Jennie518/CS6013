c826635b0befc51b71716c2dd2977a3647e99514
YIJUN <u1475897@umail.utah.edu>
2024-01-29 10:34:27 -0700

    ass1

--- ass1/main.cpp
+++ ass1/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>  // 确保包含了 iostream 头文件
 #include <sys/time.h>
-
+#include <cstring>
 extern "C" {
     void sayHello();
     int myPuts(const char* s, int len);
@@ -15,5 +15,16 @@ int main() {
     // 输出获取到的时间
     std::cout << "Seconds: " << tv.tv_sec << std::endl;
     std::cout << "Microseconds: " << tv.tv_usec << std::endl;
+    timeval tv_std;
+	gettimeofday(&tv_std, nullptr);
+	// Check if the seconds are the same or off by 1 (accounting for execution time)
+	if (tv.tv_sec == tv_std.tv_sec ||
+	  std::abs(tv.tv_sec - tv_std.tv_sec) == 1) {
+		std::cout << "Test Passed.\n";
+	} else {
+		std::cout << "Test Failed.\n";
+	}
     return 0;
 }
+
+
